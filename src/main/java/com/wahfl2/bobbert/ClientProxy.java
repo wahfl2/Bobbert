4adf9fb704aac89efdc38b45fa900ac484ede6c5
wahfl2 <59855656+wahfl2@users.noreply.github.com>
2025-10-24 14:15:02 -0500

    Register config woops

--- src/main/java/com/wahfl2/bobbert/ClientProxy.java
+++ src/main/java/com/wahfl2/bobbert/ClientProxy.java
@@ -1,5 +1,7 @@
 package com.wahfl2.bobbert;
 
+import com.gtnewhorizon.gtnhlib.config.ConfigException;
+import com.gtnewhorizon.gtnhlib.config.ConfigurationManager;
 import net.minecraftforge.common.MinecraftForge;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -8,7 +10,13 @@ public class ClientProxy extends CommonProxy {
 
     @Override
     public void preInit(FMLPreInitializationEvent event) {
-        Bobbert.LOG.info("Hello from Bobbert!");
+        Bobbert.LOG.info("Hello from Bobbert! v" + Tags.VERSION);
         MinecraftForge.EVENT_BUS.register(Bobbert.INSTANCE);
+
+        try {
+            ConfigurationManager.registerConfig(BobbertConfig.class);
+        } catch (ConfigException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
